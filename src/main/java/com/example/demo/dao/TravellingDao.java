960f964a5bd25652d9d79c21f33a44cb3bf9032e
manikanta swamy <manikantaswamy.n@qualinsys.com>
2023-09-28 19:50:48 +0530

    satya

--- src/main/java/com/example/demo/dao/TravellingDao.java
+++ src/main/java/com/example/demo/dao/TravellingDao.java
@@ -4,7 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.example.demo.model.TravellingModel;
 
-//manikanta
+//satya
 	@Repository
 	public interface TravellingDao extends JpaRepository<TravellingModel,Integer> {
 		Iterable<TravellingModel> findBySourceAndDestination(String source, String destination);
